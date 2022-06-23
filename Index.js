1e5a8366bee626289064e8d606d4440367b9ee0b
Ojomo Esther <105324040+msxtar@users.noreply.github.com>
2022-06-23 09:26:49 +0100

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,15 @@
+var opr = prompt("operator: ")
+var num1 = parseInt(prompt("Number 1: "))
+var num2 = parseInt(prompt("Number 2: "))
+if (opr== "+")
+{
+  console.log(num1 + num2)
+} else if (opr== "-")
+{console.log(num1 - num2)
+} else if (opr== "*")
+{
+  console.log(num1 * num2)
+} else if (opr== "/")
+{
+  console.log(num1 * num2)
+}
